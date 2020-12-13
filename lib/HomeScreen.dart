6bbe6bed4351631641d689da613f9ca128d1f5bd
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2020-12-13 22:43:24 +0530

    Error handling done

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -7,53 +7,76 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final data = Provider.of<WeatherApi>(context);
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: data.isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: [
-                    Center(
-                      child: Text(data.temperature.toString() + ' °C',
-                          style:
-                              TextStyle(color: Colors.white, fontSize: 60.0)),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      data.location,
-                      style: TextStyle(color: Colors.white, fontSize: 40),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Container(
-                      width: 300,
-                      child: TextField(
-                        onSubmitted: (String input) {
-                          data.fetchSearch(input);
-                        },
-                        style: TextStyle(color: Colors.white, fontSize: 17),
-                        decoration: InputDecoration(
-                            hintText: "Seach another location...",
-                            hintStyle: TextStyle(color: Colors.white),
-                            prefixIcon: Icon(
-                              Icons.search,
-                              color: Colors.white,
-                            )),
+        backgroundColor: Colors.black54,
+        body: data.isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      Center(
+                        child: Image.network(
+                          'https://www.metaweather.com//static/img/weather/png/${data.abbrevation}.png',
+                          width: 100,
+                        ),
                       ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-    );
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text(data.temperature.toString() + ' °C',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 40)),
+                      ),
+                      SizedBox(height: 30),
+                      Text(
+                        data.location,
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        width: 300,
+                        child: TextField(
+                          onSubmitted: (String input) async {
+                            try {
+                              await data.fetchSearch(input);
+                            } catch (error) {
+                              showDialog(
+                                context: context,
+                                builder: (ctx) => AlertDialog(
+                                  title:
+                                      Text('Oops Sorry for the inconvenience!'),
+                                  content: Text(
+                                      "We don't have data about this city. Try another one."),
+                                  actions: [
+                                    FlatButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text("Okay"))
+                                  ],
+                                ),
+                              );
+                            }
+                          },
+                          style: TextStyle(color: Colors.white, fontSize: 17),
+                          decoration: InputDecoration(
+                              hintText: "Seach another location...",
+                              hintStyle: TextStyle(color: Colors.white),
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Colors.white,
+                              )),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ));
   }
 }
--- lib/providers/weather_API.dart
+++ lib/providers/weather_API.dart
@@ -4,7 +4,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherApi with ChangeNotifier {
+  WeatherApi() {
+    fetchSearch("Los angeles");
+  }
+
   bool isLoading = false;
+
+  String abbrevation = '';
+
   String searchApiUrl =
       'https://www.metaweather.com/api/location/search/?query=';
 
@@ -18,12 +25,21 @@ class WeatherApi with ChangeNotifier {
 
   String weather = 'clear';
 
-  void fetchSearch(String input) async {
+  fetchSearch(String input) async {
     isLoading = true;
     notifyListeners();
+    try {
+      var searchResult = await http.get(searchApiUrl + input);
+      var result = json.decode(searchResult.body)[0];
+      location = result['title'];
+      fetchLocation(result['woeid'].toString());
+    } catch (error) {
+      isLoading = false;
+      notifyListeners();
+      throw (error);
+    }
     var searchResult = await http.get(searchApiUrl + input);
     var result = json.decode(searchResult.body)[0];
-    print(result);
     location = result['title'];
     fetchLocation(result['woeid'].toString());
     notifyListeners();
@@ -32,12 +48,12 @@ class WeatherApi with ChangeNotifier {
   void fetchLocation(String woeid) async {
     var locationResult = await http.get(locationApiUrl + woeid.toString());
     var result = json.decode(locationResult.body);
-    print(result);
     var consolidatedWeather = result['consolidated_weather'];
     var data = consolidatedWeather[0];
     temperature = data['the_temp'].round();
     weather = data['weather_state_name'].replaceAll(" ", "").toLowerCase();
-    notifyListeners();
+    abbrevation = data['weather_state_abbr'];
     isLoading = false;
+    notifyListeners();
   }
 }
